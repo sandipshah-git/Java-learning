6c397a6e6ef16f6061a5878434085ec17f36a36d
sandipsah-git <sandip123456sah@gmail.com>
2025-05-27 08:59:32 +0545

    commited

---
+++ randomNumber.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class randomNumber {
+    public static void main(String[] args){
+        Random random = new Random();
+
+        int x = random.nextInt(1,6); // it gives  1,2,3,4,5 
+
+        System.out.println(x);
+
+    }
+}
