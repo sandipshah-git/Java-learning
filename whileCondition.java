f76e95134ed229634ebd935b93dc05253e7fa70a
sandipsah-git <sandip123456sah@gmail.com>
2025-06-15 09:32:39 +0545

    use while instead of if & also make infinite loop

---
+++ whileCondition.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class whileCondition {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        //declaration
+        String name = "";
+
+        while(name.isEmpty()){ // if is not work instead of while
+            System.out.print("Enter your name : ");
+            name = scanner.nextLine();
+        }
+
+        System.out.println("Hello " + name);
+
+        // below code works for infinite loop
+
+        // while(1 == 1){
+        //     System.out.println("Help!, i'm stuck in a loop");
+        // }
+
+        scanner.close();
+    }
+}
