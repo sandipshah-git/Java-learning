5dd5bc43c0e556ed394119aec640c411f4aa1eee
sandipsah-git <sandip123456sah@gmail.com>
2025-06-19 13:12:56 +0545

    use of break and contineu

---
+++ breakContineu_24.java
@@ -0,0 +1,17 @@
+public class breakContineu_24 {
+    public static void main(String[] args){
+
+        // break = break out of a loop (stop)
+        // continue = skip corrent iteration of a loop (skip)
+
+        for(int i=1;i<=10;i++){
+            if(i==5){
+                // break;
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+
+
+    }
+}
