124341e9b574998879562dbfb9001cdd4a23e43b
sandipsah-git <sandip123456sah@gmail.com>
2025-06-20 10:12:06 +0545

    commit

---
+++ nestedForLoop_25.java
@@ -0,0 +1,13 @@
+public class nestedForLoop_25 {
+    public static void main(String[] args){
+        // nested loop = a loop inside another loop 
+        //              use often with matrices and dsa
+
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=3;j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
