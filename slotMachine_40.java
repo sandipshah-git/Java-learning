39ea1673646877c3f0dd25fcb7300c2d923b4b75
sandipsah-git <sandip123456sah@gmail.com>
2025-07-02 09:03:40 +0545

    update

--- slotMachine_40.java
+++ slotMachine_40.java
@@ -1,3 +1,12 @@
+    /*  for print emoji in terminal
+add first font family
+Segoe UI Emoji, Cascadia Code, monospace
+and then,
+run in terminal 
+chcp 65001  
+javac -encoding UTF-8 EmojiTest.java
+java -Dfile.encoding=UTF-8 EmojiTest
+ */
 import java.util.Random;
 import java.util.Scanner;
 
