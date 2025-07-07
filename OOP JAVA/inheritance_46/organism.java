46f31b1164b43bf6abf8619317f42712e57b0966
sandipsah-git <sandip123456sah@gmail.com>
2025-07-07 23:31:08 +0545

    commit

--- OOP JAVA/inheritance_46/organism.java
+++ OOP JAVA/inheritance_46/organism.java
@@ -4,5 +4,8 @@ public class organism {
 
     organism(){
         isAlive = true;
+        // System.out.println("chek");  --> to call this only do in main class
+                                        //   plant pt = new plant();  (can replace plant with organism)
+                                        //   a'm write plant because it has inherits organism class
     }
 }
